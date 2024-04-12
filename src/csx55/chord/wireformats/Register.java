b2aaa171ab972b646bf31dfd26503da7872ead48
karkidilochan <karkidilochan.dk@gmail.com>
2024-04-12 14:01:23 -0600

    fixed download failure and success cases

--- src/csx55/chord/wireformats/Register.java
+++ src/csx55/chord/wireformats/Register.java
@@ -7,8 +7,6 @@ import java.io.ByteArrayOutputStream;
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
 import java.io.Serializable;
 
 /**
